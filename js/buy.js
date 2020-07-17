b02f849537d1d9b7c0d5f0f4dd53a6c231c8af07
maruhachi <konchan1129@gmail.com>
2020-07-18 00:51:38 +0900

    feat: 購入画面の動作は作成完了.

--- js/buy.js
+++ js/buy.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
 import VueTheMask from "vue-the-mask";
-// import { ENV_VARS, COM_FUNC } from "/common/js/env.js";
 
-import * as jQuery from "jquery";
+import jQuery from "jquery";
+window.$ = window.jQuery = jQuery;
 
 (function () {
   "use strict";
@@ -10,12 +10,16 @@ import * as jQuery from "jquery";
   // ===========================================================================
   // 共通変数定義
   // ---------------------------------------------------------------------------
-
+  let izakayaId;
+  let pricePerHour;
   // ===========================================================================
   // イベントハンドラ関数定義
   // ---------------------------------------------------------------------------
 
-  var handler = {
+  var handlers = {
+    clickBuyLink: function (e) {
+      window.location.href = "/detail.html?id=" + izakayaId;
+    },
     submitPurchase: function () {
       var cardInformation = {
         name: document.querySelector("#cardName").value,
@@ -40,7 +44,7 @@ import * as jQuery from "jquery";
           purchaseFormData.append("email", emailVal);
           purchaseFormData.append("izakayaId", izakayaId);
 
-          fetch("https://localhost.re-era.tech:8443" + "/ticket/buy", {
+          fetch(process.env.AP_CONTEXT_PATH + "/ticket/buy", {
             method: "POST",
             body: purchaseFormData,
           })
@@ -63,30 +67,28 @@ import * as jQuery from "jquery";
   //  - イベントハンドラを設定
   //  - アラートを表示 etc
   // ---------------------------------------------------------------------------
-  document.addEventListener(
-    "DOMContentLoaded",
-    function () {
-      var params = new URL(document.location).searchParams;
-      var izakayaId = params.get("id");
+  var params = new URL(document.location).searchParams;
+  izakayaId = params.get("id");
 
-      console.log("id :", izakayaId);
+  console.log("id :", izakayaId);
 
-      getIzakaya(izakayaId);
+  getIzakaya(izakayaId);
 
-      Omise.setPublicKey("pkey_test_5k4953yfhskp2xwoquh");
+  Omise.setPublicKey("pkey_test_5k4953yfhskp2xwoquh");
 
-      document
-        .getElementById("purchase-btn")
-        .addEventListener("click", handler.submitPurchase);
-    },
-    false
-  );
+  document
+    .getElementById("link-out")
+    .addEventListener("click", handlers.clickBuyLink);
+  document
+    .getElementById("purchase-btn")
+    .addEventListener("click", handlers.submitPurchase);
+
   // ===========================================================================
   // 関数定義 (イベントハンドラ以外)
   // ---------------------------------------------------------------------------
 
   function getIzakaya(izakayaId) {
-    fetch("https://localhost.re-era.tech:8443/izakaya/" + izakayaId, {
+    fetch(process.env.AP_CONTEXT_PATH + "/izakaya/" + izakayaId, {
       method: "GET",
       mode: "cors",
     })
@@ -97,9 +99,10 @@ import * as jQuery from "jquery";
         return response.json();
       })
       .then((data) => {
-        console.log(data);
+        console.log(data.pricePerHour);
+        pricePerHour = data.pricePerHour;
         var vm = new Vue({
-          el: "#izakaya-content",
+          el: "#box__izakaya",
           data: {
             izakaya: [],
           },
@@ -107,100 +110,101 @@ import * as jQuery from "jquery";
             this.izakaya = data;
           },
         });
+
+        /*
+  See on github: https://github.com/muhammederdem/credit-card-form
+  */
+        Vue.use(VueTheMask);
+        new Vue({
+          el: "#app",
+          data() {
+            return {
+              // currentCardBackground: Math.floor(Math.random() * 5 + 1), // just for fun :D
+              cardName: "",
+              cardNumber: "",
+              cardMonth: "",
+              cardYear: "",
+              cardCvv: "",
+              minCardYear: new Date().getFullYear(),
+              amexCardMask: "#### ###### #####",
+              otherCardMask: "#### #### #### ####",
+              cardNumberTemp: "",
+              isCardFlipped: false,
+              focusElementStyle: null,
+              isInputFocused: false,
+              price: pricePerHour,
+            };
+          },
+          mounted() {
+            this.cardNumberTemp = this.otherCardMask;
+            this.price = pricePerHour;
+          },
+          computed: {
+            getCardType() {
+              let number = this.cardNumber;
+              let re = new RegExp("^4");
+              if (number.match(re) != null) return "visa";
+
+              re = new RegExp("^(34|37)");
+              if (number.match(re) != null) return "amex";
+
+              re = new RegExp("^5[1-5]");
+              if (number.match(re) != null) return "mastercard";
+
+              re = new RegExp("^6011");
+              if (number.match(re) != null) return "discover";
+
+              re = new RegExp("^9792");
+              if (number.match(re) != null) return "troy";
+
+              return "visa"; // default type
+            },
+            generateCardNumberMask() {
+              return this.getCardType === "amex"
+                ? this.amexCardMask
+                : this.otherCardMask;
+            },
+            minCardMonth() {
+              if (this.cardYear === this.minCardYear)
+                return new Date().getMonth() + 1;
+              return 1;
+            },
+          },
+          watch: {
+            cardYear() {
+              if (this.cardMonth < this.minCardMonth) {
+                this.cardMonth = "";
+              }
+            },
+          },
+          methods: {
+            flipCard(status) {
+              this.isCardFlipped = status;
+            },
+            focusInput(e) {
+              this.isInputFocused = true;
+              let targetRef = e.target.dataset.ref;
+              let target = this.$refs[targetRef];
+              this.focusElementStyle = {
+                width: `${target.offsetWidth}px`,
+                height: `${target.offsetHeight}px`,
+                transform: `translateX(${target.offsetLeft}px) translateY(${target.offsetTop}px)`,
+              };
+            },
+            blurInput() {
+              let vm = this;
+              setTimeout(() => {
+                if (!vm.isInputFocused) {
+                  vm.focusElementStyle = null;
+                }
+              }, 300);
+              vm.isInputFocused = false;
+            },
+          },
+        });
       })
       .catch((err) => {
         console.log(err);
       });
   }
-
-  /*
-  See on github: https://github.com/muhammederdem/credit-card-form
-  */
-  Vue.use(VueTheMask);
-  new Vue({
-    el: "#app",
-    data() {
-      return {
-        currentCardBackground: Math.floor(Math.random() * 5 + 1), // just for fun :D
-        cardName: "",
-        cardNumber: "",
-        cardMonth: "",
-        cardYear: "",
-        cardCvv: "",
-        minCardYear: new Date().getFullYear(),
-        amexCardMask: "#### ###### #####",
-        otherCardMask: "#### #### #### ####",
-        cardNumberTemp: "",
-        isCardFlipped: false,
-        focusElementStyle: null,
-        isInputFocused: false,
-      };
-    },
-    mounted() {
-      this.cardNumberTemp = this.otherCardMask;
-      document.getElementById("cardNumber").focus();
-    },
-    computed: {
-      getCardType() {
-        let number = this.cardNumber;
-        let re = new RegExp("^4");
-        if (number.match(re) != null) return "visa";
-
-        re = new RegExp("^(34|37)");
-        if (number.match(re) != null) return "amex";
-
-        re = new RegExp("^5[1-5]");
-        if (number.match(re) != null) return "mastercard";
-
-        re = new RegExp("^6011");
-        if (number.match(re) != null) return "discover";
-
-        re = new RegExp("^9792");
-        if (number.match(re) != null) return "troy";
-
-        return "visa"; // default type
-      },
-      generateCardNumberMask() {
-        return this.getCardType === "amex"
-          ? this.amexCardMask
-          : this.otherCardMask;
-      },
-      minCardMonth() {
-        if (this.cardYear === this.minCardYear)
-          return new Date().getMonth() + 1;
-        return 1;
-      },
-    },
-    watch: {
-      cardYear() {
-        if (this.cardMonth < this.minCardMonth) {
-          this.cardMonth = "";
-        }
-      },
-    },
-    methods: {
-      flipCard(status) {
-        this.isCardFlipped = status;
-      },
-      focusInput(e) {
-        this.isInputFocused = true;
-        let targetRef = e.target.dataset.ref;
-        let target = this.$refs[targetRef];
-        this.focusElementStyle = {
-          width: `${target.offsetWidth}px`,
-          height: `${target.offsetHeight}px`,
-          transform: `translateX(${target.offsetLeft}px) translateY(${target.offsetTop}px)`,
-        };
-      },
-      blurInput() {
-        let vm = this;
-        setTimeout(() => {
-          if (!vm.isInputFocused) {
-            vm.focusElementStyle = null;
-          }
-        }, 300);
-        vm.isInputFocused = false;
-      },
-    },
-  });
 })();
