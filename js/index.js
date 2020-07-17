223508388d7b2b0f579c60033022800f0fc5a598
maruhachi <konchan1129@gmail.com>
2020-07-18 00:50:12 +0900

    feat: parcelとNetlifyの環境差異を扱う仕組みに乗せる.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,2 @@
-import { ENV_VARS, COM_FUNC } from "/common/js/env.js";
-
 var el = document.getElementById("headerT1");
-el.innerHTML = "オンライン横丁(仮) " + ENV_VARS.ENV_NAME;
-
-COM_FUNC.hello();
+el.innerHTML = "オンライン横丁(仮) " + process.env.ENV_NAME;
