4965f8eb6fc2ae28375f52eaa200ca0f18e085e9
maruhachi <konchan1129@gmail.com>
2020-11-09 22:21:00 +0900

    feat: 外からコンテンツのバージョンを特定する仕組みを導入.

--- src/common/js/common.js
+++ src/common/js/common.js
@@ -1,23 +1,9 @@
-// 上記のようにすることで名前空間を限定できる
 
 "use strict";
 
 // ---------------------------------------------------------------------------
 // 共通変数定義
 // ---------------------------------------------------------------------------
-
-// ---------------------------------------------------------------------------
-// イベントハンドラ関数定義
-// ---------------------------------------------------------------------------
-
-/**
- * イベントハンドラ
- */
-function onClickBurgerToggle(e) {
-  console.log("test");
-  document.querySelector(".navbar-burger").classList.toggle("is-active");
-  document.querySelector(".navbar-menu").classList.toggle("is-active");
-}
 
 // ---------------------------------------------------------------------------
 // 初期化関数定義 (ページの DOM ツリー構築完了後に行う初期処理)
@@ -28,39 +14,34 @@ function onClickBurgerToggle(e) {
  * - イベントハンドラを設定する。
  * - アラートを表示する。
  */
-$(function init() {
+(function () {
 
-  // var burgerBtn = document.getElementById("toggleBurger");
-  // burgerBtn.addEventListener("click", handlers.onClickBurgerToggle, false);
-
-  // TODO:ひとまず全pをフェードイン
-  // const fadeTargets = document.querySelectorAll("p");
-  // fadeTargets.forEach(function (target) {
-  // 監視の開始
-  // target.classList.add("fadein");
-  // });
-
+  // ===== スクロール時にふわっと現れる演出スクリプト ======
   // IntersectionObserverオブジェクトを作成する。
   // 交差時に実行するコールバック関数を渡す。
+
+  // しきい値を viewport に要素の高さ1%が入っていることする
   const options = { threshold: [0.01] };
 
   const observer = new IntersectionObserver((entries) => {
     for (const e of entries) {
-      // 交差していない
+      // 交差していないならskip
       if (!e.isIntersecting) continue;
 
       e.target.classList.remove("unscroll");
     }
   }, options);
 
-  // 監視したい要素をobserveする。
+  // 監視対象である特定class要素をobserveする。
   const targets = document.querySelectorAll(".fade.unscroll");
   targets.forEach(function (target) {
-    // 監視の開始
+    // 監視を開始する.
     observer.observe(target);
   });
-});
 
-// ---------------------------------------------------------------------------
-// 関数定義 (イベントハンドラ以外)
-// ---------------------------------------------------------------------------
+  // ===== コンテンツのバージョン確認用スクリプト ======
+  // htmlのdata属性にビルド時のコミットIdを埋め込む
+  const htmlContent = document.querySelector("html");
+  htmlContent.dataset.appCommitId = process.env.COMMIT_REF;
+
+})();
